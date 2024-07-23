96442ba112c1588ef083e30a9bcc570216ea6903
G2OUZ <101153474+avelinonscorp@users.noreply.github.com>
2024-07-22 23:40:57 -0400

    Update loggin functionalities and enable github actions

--- src/main/java/com/business/unknown/APIHandler.java
+++ src/main/java/com/business/unknown/APIHandler.java
@@ -7,6 +7,8 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,21 +18,22 @@ import java.io.OutputStreamWriter;
 
 public class APIHandler implements RequestStreamHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(APIHandler.class);
     private JSONParser parser = new JSONParser();
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-        LambdaLogger logger = context.getLogger();
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         JSONObject responseJson = new JSONObject();
         try {
             JSONObject event = (JSONObject) parser.parse(reader);
 
             if (event.get("body") != null) {
-                logger.log(event.get("body").toString());
+                logger.info("BODY: "+event.get("body").toString());
             }
 
             JSONObject responseBody = new JSONObject();
-            responseBody.put("message", "lambda example");
+            responseBody.put("message", "lambda example automatic deploy with github actions");
 
             JSONObject headerJson = new JSONObject();
             headerJson.put("test", " my test header");
